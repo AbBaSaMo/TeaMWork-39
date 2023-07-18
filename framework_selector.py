167f16a39d53c6352e1230231b11528b56dcc2b1
Baaset Moslih <s.a.baaset.moslih@gmail.com>
2023-07-19 07:05:20 +1000

    refactored data preprocessing into function, added prediction

--- framework_selector.py
+++ framework_selector.py
@@ -7,6 +7,40 @@ from sklearn.tree import DecisionTreeClassifier
 # from sklearn.tree import DecisionTreeClassifier
 # from sklearn.metrics import accuracy_score
 
+
+def preprocess_data(data):
+    # group columns by the pre-processing that must be done
+    categorical_columns = ['IndustryName', 'Microsoft', 'Country']
+    ranged_columns = ['AnnualTurnover', 'CyberSecurityBudget', 'Staff', 'RiskImpactLevel']
+    set_columns = ['BusinessRoles', 'InformationHoldings', 'SecurityTargets']
+
+    # convert all nominal-categorical data using label encoding
+    for column in categorical_columns:
+        data[column] = LabelEncoder().fit_transform(data[column])
+
+    # convert all set type data using one-hot encoding
+    for column in set_columns:
+        # convert the comma delimited string of items to a list i.e. 'A, B, C' -> ['A', 'B', 'C']
+        data[column] = data[column].str.split(',')
+
+        # convert the list to binary format
+        enc_data = pd.get_dummies(
+            data[column]
+            .apply(pd.Series)
+            .stack()
+        ).groupby(level=0).sum()
+
+        # add this data to the training data [each item in the list is added as a column] and remove the old format
+        data = pd.concat([data, enc_data], axis=1)
+        data = data.drop(column, axis=1)
+
+    # convert all ranged type data
+    for column in ranged_columns:
+        data[column] = LabelEncoder().fit_transform(data[column])
+
+    return data
+
+
 frameworks = {
     'APAC APS CPS234': 1,
     'APAC Malaysia PDPA': 2,
@@ -27,52 +61,26 @@ testing_data = pd.read_excel('testing_data.xlsx')
 training_data_X = training_data.drop('Primary recommendation', axis=1).drop('Name', axis=1)
 training_data_Y = training_data['Primary recommendation']
 
+company_names = testing_data['Name']
+testing_data = testing_data.drop('Name', axis=1)
+
 # PRE-PROCESSING OF TRAINING AND TEST DATA
-enc_training_data_X = training_data_X.copy()
-enc_training_data_Y = training_data_Y.copy()
-
-# group columns by the pre-processing that must be done
-categorical_columns = ['IndustryName', 'Microsoft', 'Country']
-ranged_columns = ['AnnualTurnover', 'CyberSecurityBudget', 'Staff', 'RiskImpactLevel']  # average, scale or categorise
-set_columns = ['BusinessRoles', 'InformationHoldings', 'SecurityTargets']
-
-
-# convert all nominal-categorical data using label encoding
-enc_training_data_Y = LabelEncoder().fit_transform(enc_training_data_Y)
-for column in categorical_columns:
-    enc_training_data_X[column] = LabelEncoder().fit_transform(enc_training_data_X[column])
-
-# convert all set type data using one-hot encoding
-for column in set_columns:
-    # convert the comma delimited string of items to a list i.e. 'A, B, C' -> ['A', 'B', 'C']
-    enc_training_data_X[column] = enc_training_data_X[column].str.split(',')
-
-    # convert the list to binary format
-    enc_data = pd.get_dummies(
-        enc_training_data_X[column]
-        .apply(pd.Series)
-        .stack()
-    ).groupby(level=0).sum()
-
-    # add this data to the training data [each item in the list is added as a column] and remove the old format
-    enc_training_data_X = pd.concat([enc_training_data_X, enc_data], axis=1)
-    enc_training_data_X = enc_training_data_X.drop(column, axis=1)
-
-# convert all ranged type data
-for column in ranged_columns:
-    enc_training_data_X[column] = LabelEncoder().fit_transform(enc_training_data_X[column])
-
-print(enc_training_data_X)
+label_encoder = LabelEncoder()
+enc_training_data_Y = label_encoder.fit_transform(training_data_Y.copy())
+enc_training_data_X = preprocess_data(training_data_X.copy())
+enc_testing_data = preprocess_data(testing_data.copy())
 
 # TRAIN AND TEST THE MODEL
 model = DecisionTreeClassifier()
 model.fit(enc_training_data_X, enc_training_data_Y)
 
-prediction = model.predict(feature_test)
-decoded_prediction = encoder.inverse_transform(prediction)
-# for pred in decoded_prediction:
+prediction = model.predict(enc_testing_data)
+prediction = label_encoder.inverse_transform(prediction)
 
-# # todo verify model with the testing data
+for framework, company in zip(prediction, company_names):
+    print(company + ' -> ' + framework)
+
+# verify model with the testing data
 # print(prediction)
 # accuracy = accuracy_score(target_test, prediction)
 # print(accuracy)
